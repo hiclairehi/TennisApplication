376c29b1ccc3f9bd3a3329a92a025c3fd65b54bc
Yousra Bouchair <yusarin200215@gmail.com>
2023-05-17 09:01:05 +0200

    Changes in the login Interface
    chnaged the login page but it needs an Tennis racket icon

--- IPC_FXMLCore-master/src/controller/LoginController.java
+++ IPC_FXMLCore-master/src/controller/LoginController.java
@@ -8,6 +8,11 @@ package controller;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,11 +20,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.Club;
+import model.ClubDAOException;
+import model.Court;
 
 
 /**
@@ -27,17 +36,24 @@ import model.Club;
  * @author jsoler
  */
 public class LoginController implements Initializable {
-    //private Label labelMessage;
-    @FXML
-    private Button signUp;
     @FXML
     private TextField nickname;
+    
+    private BooleanProperty validNickname;
+    
     @FXML
     private PasswordField password;
-    @FXML
-    private Button login;
     
     private Club club;
+    @FXML
+    private Button add;
+    @FXML
+    private Label errorMessage;
+    
+    
+    
+    
+    
     
     
     //=========================================================
@@ -50,24 +66,59 @@ public class LoginController implements Initializable {
     // you must initialize here all related with the object 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-
-//    @FXML
-//    private void signUp(ActionEvent event) throws IOException {
-//        FXMLLoader myFXMLloader= new FXMLLoader(getClass().getResource("SignUpFXML"));
-//        Parent root= myFXMLloader.load();
-//        SignUpController signup = myFXMLloader.getController();
-//        Scene scene= new Scene(root,500,300);
-//        Stage stage=new Stage();
-//        stage.setTitle("Sign up");
-//        stage.initModality(Modality.APPLICATION_MODAL);
-//        stage.show();
-//        
+        try {
+            club= Club.getInstance();
+            
+        } catch (ClubDAOException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            
+            add.disableProperty().bind(Bindings.and(nickname.textProperty().isEmpty(), password.textProperty().isEmpty()));
         
+            
+                
+        
+    }
+
+     
+
+  
         
         
 //        
 //    }
+
+    @FXML
+    private void signUp(ActionEvent event) throws IOException {
+        FXMLLoader myFXMLloader= new FXMLLoader(getClass().getResource("/views/SignUpFXML.fxml"));
+        Parent root= myFXMLloader.load();
+        SignUpController signup = myFXMLloader.getController();
+        Scene scene= new Scene(root,500,300);
+        Stage stage=new Stage();
+        stage.setTitle("Sign up");
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.show();
+    }
+
+    @FXML
+    private void login(ActionEvent event) {
+        // check if the user is Member of the club
+        if(!club.existsLogin(nickname.textProperty().getValueSafe())){
+            errorMessage.setVisible(true);
+            nickname.clear();
+            password.clear();
+        } else{
+            // change window to the home page
+        }
+    }
+
+    @FXML
+    private void availability(ActionEvent event) {
+        
+    }
+    
+    
     
 }
